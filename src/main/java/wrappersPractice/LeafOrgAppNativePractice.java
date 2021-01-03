6534076d1c2475e78b97a20f8d6c392ecd405d26
Thyagarajan, Lokesh Kumar <lokeshkumar.thyagarajan@hidglobal.com>
2021-01-03 09:10:09 +0530

    Code updated

--- src/main/java/wrappersPractice/LeafOrgAppNativePractice.java
+++ src/main/java/wrappersPractice/LeafOrgAppNativePractice.java
@@ -4,13 +4,13 @@ import org.testng.annotations.Test;
 
 import wrappers.GenericWrappers;
 
-public class LeafOrgAppPractice extends GenericWrappers{
+public class LeafOrgAppNativePractice extends GenericWrappers{
 	
 	@Test
 	public void executeTest() {
 		launchAndroidApp("Android phone", "com.testleaf.leaforg", "com.testleaf.leaforg.MainActivity", "", "");
 		
-		enterValue(getWebElement("xpath", "(//android.widget.EditText)[1]"), "sakthivel@testleaf.com");
+		enterValue(getWebElement("xpath", "(//android.widget.EditText)[1]"), "rajkumar@testleaf.com");
 		enterValue(getWebElement("xpath", "(//android.widget.EditText)[2]"), "Leaf@123");
 		click(getWebElement("xpath", "//android.widget.Button"));
 	}
