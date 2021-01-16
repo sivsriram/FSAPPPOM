f21102fd32222f261c123b275fa369afe8b2caf5
Thyagarajan, Lokesh Kumar <lokeshkumar.thyagarajan@hidglobal.com>
2021-01-16 19:54:00 +0530

    Code updated

--- src/main/java/wrappersPractice/IosWebViewPractice.java
+++ src/main/java/wrappersPractice/IosWebViewPractice.java
@@ -3,22 +3,18 @@ package wrappersPractice;
 import org.testng.annotations.Test;
 
 import wrappers.GenericWrappers;
-import wrappers.CommonWrappers.Locators;
 
-public class IosWebViewPractice  extends GenericWrappers{
+public class IosWebViewPractice extends GenericWrappers {
 
 	@Test
 	public void runTC() {
-		launchIosApp("iPhone 8", "119ACEC8-470E-48D6-ABD1-DC78D4AC70F6", "", "com.example.apple-samplecode.UICatalog", "");
+		launchIosApp("iPhone 8", "119ACEC8-470E-48D6-ABD1-DC78D4AC70F6", "", "com.example.apple-samplecode.UICatalog",
+				"");
 		scrollFromDownToUpinAppUsingPointerInputUntilElementIsVisible(Locators.NAME.toString(), "Web View");
 		click(getWebElement(Locators.NAME.toString(), "Web View"));
 		sleep(10000);
 		printContext();
 		switchWebview();
 		click(getWebElement(Locators.XPATH.toString(), "//li[@class='ac-gn-item ac-gn-menuicon']/a"));
-		
-		
-		
 	}
-
 }
