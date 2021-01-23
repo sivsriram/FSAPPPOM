f928c5867b7baaf61626ce4ac765bbf23a7e6c32
Thyagarajan, Lokesh Kumar <lokeshkumar.thyagarajan@hidglobal.com>
2021-01-23 11:55:34 +0530

    Page factory learning

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,10 +1,14 @@
 package pages;
 
 import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.support.PageFactory;
 
 import com.aventstack.extentreports.ExtentTest;
 
 import io.appium.java_client.AppiumDriver;
+//import io.appium.java_client.pagefactory.AndroidFindBy;
+//import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+//import io.appium.java_client.pagefactory.iOSXCUITFindBy;
 import wrappers.GenericWrappers;
 
 public class HomePage extends GenericWrappers {
@@ -12,5 +16,12 @@ public class HomePage extends GenericWrappers {
 	public HomePage(AppiumDriver<WebElement> driver, ExtentTest test) {
 		this.driver = driver;
 		this.test = test;
+		//PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
+	
+//	@AndroidFindBy(id="")
+//	@iOSXCUITFindBy(className="")
+//	private WebElement ele;
+	
+	
 }
